ad17d8fb81f534e2c3a7dbd99579a3f3dd7a4f9a
Lucia Mormocea <lucia.mormocea@bbc.co.uk>
2020-12-08 15:21:31 +0000

    Day 1 part 2

--- day1/report-repair.py
+++ day1/report-repair.py
@@ -8,27 +8,45 @@ def read_input():
 
     return numbers
 
+def find_two_numbers(numbers, index, expectedSum):
+    i = index
+
+    top = 0
+    bottom = 0
+
+    while i > 0 and bottom == 0:
+        top = numbers[i]
+        difference = int(expectedSum - top)
+        j = 0
+        while j < i and numbers[j] < difference:
+            j += 1
+        if numbers[j] == difference:
+            bottom = numbers[j]
+            print(top)
+            print(bottom)
+        i -= 1
+    
+    return top * bottom
+
+
+
 expectedSum = 2020
 numbers = read_input()
 numbers.sort()
 
 i = len(numbers) - 1
 
-top = 0
-bottom = 0
+first = 0
+multiplicationResult = 0
 
-while i > 0 and bottom == 0:
-    top = numbers[i]
-    difference = int(expectedSum - top)
-    j = 0
-    while j < i and numbers[j] < difference:
-        j += 1
-    if numbers[j] == difference:
-        bottom = numbers[j]
-        print(top)
-        print(bottom)
+while i > 0 and multiplicationResult == 0:
+    first = numbers[i]
+    difference = int(expectedSum - first)
+    multiplicationResult = find_two_numbers(numbers, i - 1, difference)
+    if multiplicationResult > 0:
+        print(first)
     i -= 1
 
-print( top * bottom )
+print( first * multiplicationResult )
 
 
